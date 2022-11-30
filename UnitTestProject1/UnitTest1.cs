b7746a1bfae8274c7dd1c02ba262a5091b30e16e
nikitka.nikitin.02@internet.ru <nikitka.nikitin.02@internet.ru>
2022-11-30 10:02:42 +0300

    s

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tortuga_Bezrukov;
+using Tortuga_Bezrukov.ClassHelper;
+using static Tortuga_Bezrukov.ClassHelper.CostDisc;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestCostBigger()
+        {
+            decimal cost = 10000;
+            DateTime date = new DateTime(2022, 10, 29);
+            decimal ex = 8900;
+            decimal res = Tortuga_Bezrukov.ClassHelper.CostDisc.Sum(date, cost);
+            Assert.AreEqual(res, ex);
+        }
+
+        [TestMethod]
+        public void TestCostSmaller()
+        {
+            decimal cost = 200;
+            DateTime date = new DateTime(2022, 10, 29);
+            decimal ex = 178;
+            decimal res = Tortuga_Bezrukov.ClassHelper.CostDisc.Sum(date, cost);
+            Assert.AreEqual(res, ex);
+        }
+
+        [TestMethod]
+        public void TestWrongDate()
+        {
+            decimal cost = 100;
+            DateTime date = new DateTime(2022, 10, 28);
+            decimal ex = 100;
+            decimal res = Tortuga_Bezrukov.ClassHelper.CostDisc.Sum(date, cost);
+            Assert.AreEqual(res, ex);
+        }
+    }
+}
